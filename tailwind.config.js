62a05bd8e4039393a723396ffb89d6218e281a49
Hilman-F <muhammadhilmanfairuzi@gmail.com>
2025-10-20 11:53:23 +0700

    Update Delay

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 export default {
   content: [
     "./index.html", 
-    "./src/**/*.{js,ts,jsx,tsx}",
-    // "./node_modules/animate.css/animate.css"
+    "./src/**/*.{js,ts,jsx,tsx}"
 ],
   theme: {
     extend: {
